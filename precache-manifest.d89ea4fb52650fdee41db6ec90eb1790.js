677bd48ffea8096967665df4d44e52cf8ca91e91
Ruth Amaya <ruthamaya91@gmail.com>
2020-06-22 15:20:35 -0500

    Updates

---
+++ precache-manifest.d89ea4fb52650fdee41db6ec90eb1790.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f93c6462f3896aa47082f00be80762f",
+    "url": "/amayaDev/index.html"
+  },
+  {
+    "revision": "e2f660abaf5d991b0977",
+    "url": "/amayaDev/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "14e6cb77661250122b53",
+    "url": "/amayaDev/static/js/2.f8e6fee6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/amayaDev/static/js/2.f8e6fee6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2f660abaf5d991b0977",
+    "url": "/amayaDev/static/js/main.b4f22202.chunk.js"
+  },
+  {
+    "revision": "3169bd604a74452c6d69",
+    "url": "/amayaDev/static/js/runtime-main.8275b01c.js"
+  }
+]);
